22701d22855616313b233ac18f80c95ef4270dbc
yangziqiu001012’ <894279633@qq.com>
2022-11-26 10:32:47 +0800

    ''

--- js/setRem.js
+++ js/setRem.js
@@ -7,9 +7,7 @@ function setRem() {
     //屏幕最大1920
     clientWidth = clientWidth > 1920 ? 1920 : clientWidth;
     clientWidth = clientWidth < 1024 ? 1024 : clientWidth;
-
-    //我的电脑  屏幕尺寸 1440  为了让每一个rem等于80px  /18
-    var rem = clientWidth / 18;
+    var rem = clientWidth / 24;
 
     var html = document.getElementsByTagName('html')[0];
     html.style.fontSize = rem + 'px';
